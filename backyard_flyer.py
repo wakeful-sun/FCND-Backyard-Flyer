6c2b1b1b48c9a075f1640145e0e45affd4ca1789
Oleksandr Bondarchuk <al.v.bondarchuk@gmail.com>
2021-02-14 12:50:18 +0100

    Update backyard_flyer.py
    removed one extra empty line

--- backyard_flyer.py
+++ backyard_flyer.py
@@ -179,7 +179,6 @@ class BackyardFlyer(Drone):
         self.stop_log()
 
 
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--port', type=int, default=5760, help='Port number')
